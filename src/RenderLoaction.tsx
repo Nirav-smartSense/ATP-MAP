8ad94854a0c8e2e416438b23fb49583c5cd96cd8
smart-Nirav <nirav.joshi@smartsensesolutions.com>
2023-07-07 13:04:19 +0530

    changes added

--- src/RenderLoaction.tsx
+++ src/RenderLoaction.tsx
@@ -1,12 +1,27 @@
 import { AssetTypes } from "./App";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import CustomMarker from "./CustomMarker";
+import { useEffect, useState } from "react";
+import { useMap } from "react-leaflet";
 
 const RenderLoaction = ({
   locationData,
 }: {
   locationData: AssetTypes[] | null | undefined;
 }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (locationData) {
+      locationData.forEach((locations) => {
+        map.openPopup("<div>NNN</div>", [
+          locations.locations[0].latitude,
+          locations.locations[0].longitude,
+        ]);
+      });
+    }
+  }, [locationData]);
+
   if (!locationData) {
     return null;
   }
